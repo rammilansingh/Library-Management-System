b8edd7253bb10385fd745348e1c2677a8095e6cd
rammilansingh <rammilansinghh@gmail.com>
2026-08-21 13:09:30 +0530

    Update auth.controller.js

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -6,12 +6,13 @@ import crypto from "crypto";
 import sendEmail from "../utils/sendEmails.js";
 
 // cookie options  
-const cookieOPtions={
-    httpOnly:true,
-    secure:false,
-    sameSite:"strict",
+
+const cookieOPtions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000
-}
+};
 
 // Student Register
 
@@ -137,10 +138,12 @@ export const loginUser = async(req,res)=>{
 
 export const logoutUser = async(req,res)=>{
     try {
-        res.cookie("token","",{
-            httpOnly:true,
-            expires:new Date(0)
-        })
+       res.cookie("token", "", {
+       httpOnly: true,
+       secure: true,
+       sameSite: "none",
+     expires: new Date(0)
+     });
 
         return res.status(200).json({
             success:true,
